0806d1e1bba3164c3904dc8876531db4ff19f12b
ecullu <ecullu@pros.com>
2021-05-18 16:19:15 -0500

    first commit

---
+++ src/SearchParams.js
@@ -0,0 +1,94 @@
+import { useState, useEffect, useContext } from "react";
+
+import ThemeContext from "./ThemeContext";
+import Results from "./Results";
+import useBreedList from "./useBreedList";
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
+  const [breeds] = useBreedList(animal);
+  const [theme, setTheme] = useContext(ThemeContext);
+
+  useEffect(() => {
+    requestPets();
+  }, []); //eslint-disable-line react-hooks/exhaustive-deps
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
+
+  return (
+    <div className="search-params">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
+        <label htmlFor="location">Location</label>
+        <input
+          id="location"
+          onChange={(e) => setLocation(e.target.value)}
+          value={location}
+          placeholder="Location"
+        />
+        <label htmlFor="animal">Animal</label>
+        <select
+          id="animal"
+          value={animal}
+          onChange={(e) => setAnimal(e.target.value)}
+          onBlur={(e) => setAnimal(e.target.value)}
+        >
+          <option />
+          {ANIMALS.map((animal) => (
+            <option value={animal} key={animal}>
+              {animal}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="breed">Breed</label>
+        <select
+          id="breed"
+          value={breed}
+          onChange={(e) => setBreed(e.target.value)}
+          onBlur={(e) => setBreed(e.target.value)}
+        >
+          <option />
+          {breeds.map((breed) => (
+            <option value={breed} key={breed}>
+              {breed}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="theme">
+          Theme
+          <select
+            value={theme}
+            onChange={(e) => setTheme(e.target.value)}
+            onBlur={(e) => setTheme(e.target.value)}
+          >
+            <option value="darkblue">Dark Blue</option>
+            <option value="peru">Peru</option>
+            <option value="Magenta">Magenta</option>
+            <option value="chartreuse">Cartreuse</option>
+          </select>
+        </label>
+        <button style={{ backgroundColor: theme }}>Submit</button>
+      </form>
+      <Results pets={pets} />
+    </div>
+  );
+};
+
+export default SearchParams;
